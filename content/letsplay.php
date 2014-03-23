c8a1994f5f5e6e79644e7746257f3db704b88239
MatSeifert <matthe.seifert@gmail.com>
2014-03-24 00:59:21 +0100

    Kleine Änderungen an der Lets Play Datei

--- content/letsplay.php
+++ content/letsplay.php
@@ -13,51 +13,75 @@
 		Unsere Projekte
 	</span>
 
-	<div class="LPBox">
-		<img src="images/lpPage_fnv.png" class="LetsPlayThumb" alt="Fallout: New Vegas">
-		FALLOUT NEW VEGAS <br />
-		Status: aktiv <br />
-		Spieler: Kakadu, Behemoth <br />
+	<div class="LPBlock">
+		<div class="LPBox">
+			<img src="images/lpPage_fnv.png" class="LetsPlayThumb" alt="Fallout: New Vegas">
+			<span class="LPHeadline">FALLOUT NEW VEGAS</span> <br />
+			<span class="LPContent">
+				Status: aktiv <br />
+				Spieler: Kakadu, Behemoth <br />
+			</span>
+		</div>
+
+		<div class="LPBox">
+			<img src="images/lpPage_bl.png" class="LetsPlayThumb" alt="Borderlands">
+			<span class="LPHeadline">BORDERLANDS</span> <br />
+			<span class="LPContent">
+				Status: aktiv <br />
+				Spieler: Kakadu, Fallen Angel, Behemoth <br />
+			</span>
+		</div>
+
+		<div class="LPBox">
+			<img src="images/lpPage_lanoire.png" class="LetsPlayThumb" alt="L.A. Noire">
+			<span class="LPHeadline">L.A. NOIRE</span> <br />
+			<span class="LPContent">
+				Status: aktiv <br />
+				Spieler: Kakadu, Behemoth <br />
+			</span>
+		</div>
+
+		<div class="LPBox">
+			<img src="images/lpPage_metro2033.png" class="LetsPlayThumb" alt="Metro 2033">
+			<span class="LPHeadline">METRO 2033 </span><br />
+			<span class="LPContent">
+				Status: beendet (32 Folgen)<br />
+				Spieler: Kakadu, Behemoth <br />
+			</span>
+		</div>
+
+		<div class="LPBox">
+			<img src="images/lpPage_penOver.png" class="LetsPlayThumb" alt="Penumbra Overture">
+			<span class="LPHeadline">PENUMBRA OVETURE</span><br />
+			<span class="LPContent">
+				Status: beendet (27 Folgen)<br />
+				Spieler: Kakadu, Behemoth <br />
+			</span>
+		</div>
+
+		<div class="LPBox">
+			<img src="images/lpPage_penBlaPlag.png" class="LetsPlayThumb" alt="Penumbra Black Plague">
+			<span class="LPHeadline">PENUM. BLACK PLG.</span><br />
+			<span class="LPContent">
+				Status: beendet (31 Folgen)<br />
+				Spieler: Kakadu, Behemoth <br />
+			</span>
+		</div>
+
+		<div class="LPBox">
+			<img src="images/lpPage_slender.png" class="LetsPlayThumb" alt="Slender">
+			<span class="LPHeadline">SLENDER</span><br />
+			<span class="LPContent">
+				Status: beendet (5 Folgen)<br />
+				Spieler: Kakadu, Behemoth <br />
+			</span>
+		</div>
 	</div>
 
-	<div class="LPBox">
-		<img src="images/lpPage_bl.png" class="LetsPlayThumb" alt="Borderlands">
-		BORDERLANDS <br />
-		Status: aktiv <br />
-		Spieler: Kakadu, Fallen Angel, Behemoth <br />
+	<div>
+		<p> &nbsp; </p>
+		<span class="smallHeadline">
+			Neuste Videos
+		</span>
 	</div>
-
-	<div class="LPBox">
-		<img src="images/lpPage_lanoire.png" class="LetsPlayThumb" alt="L.A. Noire">
-		L.A. NOIRE <br />
-		Status: aktiv <br />
-		Spieler: Kakadu, Behemoth <br />
-	</div>
-
-	<div class="LPBox">
-		<img src="images/lpPage_metro2033.png" class="LetsPlayThumb" alt="Metro 2033">
-		METRO 2033 <br />
-		Status: beendet (32 Folgen)<br />
-		Spieler: Kakadu, Behemoth <br />
-	</div>
-
-	<div class="LPBox">
-		<img src="images/lpPage_penOver.png" class="LetsPlayThumb" alt="Penumbra Overture">
-		PENUMBRA OVETURE<br />
-		Status: beendet (27 Folgen)<br />
-		Spieler: Kakadu, Behemoth <br />
-	</div>
-
-	<div class="LPBox">
-		<img src="images/lpPage_penBlaPlag.png" class="LetsPlayThumb" alt="Penumbra Black Plague">
-		PENUM. BLACK PLAGUE<br />
-		Status: beendet (31 Folgen)<br />
-		Spieler: Kakadu, Behemoth <br />
-	</div>
-
-	<p> &nbsp; </p>
-	<span class="smallHeadline">
-		Neuste Videos
-	</span>
-
 </div>
