65ffbd01d5d1bc74ca8002c90a9f7751ed74aa57
MatSeifert <matthe.seifert@gmail.com>
2014-06-06 00:34:15 +0200

    News werden aus einer Datenbank gelesen

--- content/news.php
+++ content/news.php
@@ -1,4 +1,38 @@
 <?php
+	// Falls die Seite lokal aufgerufen wird, wird die lokale Debug Umgebung aktiviert
+	if ($_SERVER['SERVER_NAME'] == 'localhost')
+	{
+		define('DEBUG', true);
+	}
+	else define('DEBUG', false);
+
+	function ConnectToDatabase() {
+		if (DEBUG) {
+			$database=mysqli_connect("localhost","news","6F5PHPTGKaPh7Gnf","webseite");
+			// Check connection
+			if (mysqli_connect_errno()) {
+			  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
+		}
+
+		$result = mysqli_query($database,"SELECT * FROM articles ORDER BY date DESC LIMIT 0, 10");
+
+		while($row = mysqli_fetch_array($result)) {
+		  echo '<div class="SqlNewsBox">';
+		  	echo '<a href="?site=read&id=' . $row['id'] . '"><img class="SqlNewsThumbnail" src="images/articles/thumbnails/' . $row['game'] . '.jpg" alt="' . $row['game'] . '"></a>';
+		  	echo '<span class="SqlNewsHeadline"><a href="?site=read&id=' . $row['id'] . '">' . utf8_encode(strtoupper($row['headline'])) . '</a></span>';
+		  	echo '<span class="SqlNewsDate">' . $row['date'] . '&nbsp;' . substr($row['timestamp'], 0, -3) . '&nbsp;Uhr&nbsp;von&nbsp;' . $row['author'];
+		  	echo '<span class="SqlNewsSnippet">' . utf8_encode($row['content']) . '</span>';
+		  	echo '<span class="SqlNewsReadMore"><a href="?site=read&id=' . $row['id'] . '">mehr lesen ...</a></span>';
+		  echo "</div>";
+		}
+
+
+
+		mysqli_close($database);
+
+	}
+	/*
 	function GetNewsFromRss () {
 		$newsfeed='http://vmp-clan.tumblr.com/rss';
 		$xml = simplexml_load_file(rawurlencode($newsfeed));
@@ -15,7 +49,7 @@
 			
 			$i++;
 		}
-	}
+	} */
 ?>
 <div class="whereAmI">
     NEWS
@@ -27,7 +61,7 @@
 <div class="PostPost">
 
 	<div class="LPBlock">
-		<?php GetNewsFromRss(); ?>
+		<?php ConnectToDatabase(); ?>
 	</div>
 	<p>&nbsp;</p>
 </div>
--- wbb/upload/wcf/config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbHost = 'localhost';
-$dbUser = 'root';
-$dbPassword = '';
-$dbName = 'wcf';
-$dbCharset = 'utf8';
-$dbClass = 'MySQLDatabase';
-if (!defined('WCF_N')) define('WCF_N', 1);
-?>
