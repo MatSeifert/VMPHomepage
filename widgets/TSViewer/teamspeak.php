4aa217907d0dd8e210a669d1620228e4ede687b9
Robert Lehmann <lehmrob@gmail.com>
2015-08-07 21:51:02 +0200

    Remove sources of TSViewer

--- widgets/TSViewer/teamspeak.php
+++ widgets/TSViewer/teamspeak.php
@@ -1,15 +1,15 @@
 <p>&nbsp;</p>
 
-<?php 
-	require_once("tsstatus.php"); 
-	$tsstatus = new TSStatus("ts.vmp-clan.de", 10011, 1); 
-	$tsstatus->imagePath = "widgets/TSViewer/img/"; 
-	$tsstatus->showNicknameBox = false; 
-	$tsstatus->showPasswordBox = false; 
-	$tsstatus->decodeUTF8 = true; 
+<?php
+	require_once("../../vendor/sebastien/tsstatus/tsstatus.php");
+	$tsstatus = new TSStatus("ts.vmp-clan.de", 10011, 1);
+	$tsstatus->imagePath = "vendor/sebastien/tsstatus/img/";
+	$tsstatus->showNicknameBox = false;
+	$tsstatus->showPasswordBox = false;
+	$tsstatus->decodeUTF8 = true;
 	$tsstatus->timeout = 2;
 
-	echo $tsstatus->render(); 
+	echo $tsstatus->render();
 ?>
 
 <p>&nbsp;</p>
