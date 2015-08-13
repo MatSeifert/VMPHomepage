068c7dede526e36623c384d2532b0f54542e46a0
Robert Lehmann <lehmrob@gmail.com>
2015-08-13 22:16:48 +0200

    [WIP] Begin configuration for nightly deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,41 @@
 module.exports = function(grunt) {
-	grunt.initConfig({})
-	
-	grunt.registerTask('default', function() {
-		console.log("Starting the default task");
-	});
+grunt.initConfig({
+	clean: {
+		build: ["vendor"]
+	},
+	sshconfig: {
+  		"vmp-clan": grunt.file.readJSON('secret.json')
+	},
+	/*sftp: {
+		nightly: {
+
+		}
+	}*/
+	sshexec: {
+		uptime: {
+			command: 'uptime',
+	    	options: {
+				config: 'vmp-clan'
+	    	}
+		},
+		clean_nightly: {
+			command: 'rm -rf /var/www/nightly',
+			options: {
+				config: 'vmp-clan'
+	    	}
+		},
+		save_forum_nightly: {
+			command: 'mv /var/www/wbb /tmp/wbb',
+			options: {
+				config: 'vmp-clan'
+			}
+		}
+	}
+});
+
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-ssh');
+
+grunt.registerTask('nightly_clean', ['sshexec:save_forum_nightly',
+	'sshexec:clean_nightly']);
 };
